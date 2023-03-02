c085b69452eb94f20fa2aa2e86c0ffffb094c77d
muthumurugan <muthumurugan.ramalingam@hirepro.in>
2023-03-02 14:57:07 +0530

    added new io_user_directory to work in all system paths

---
+++ SCRIPTS/COMMON/io_user_directory.py
@@ -0,0 +1,5 @@
+# This is the local user directory, should be ignored while pushing to git repo
+
+input_common_dir = 'F:\\qa_automation\\PythonWorkingScripts_InputData\\'
+output_common_dir = 'F:\\qa_automation\\PythonWorkingScripts_Output\\'
+chrome_driver_dir = 'F:\\qa_automation\\'
