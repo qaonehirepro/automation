29d811e8e3646a596ac9e8b16d858ffb0d88f9f2
muthumurugan <muthumurugan.ramalingam@hirepro.in>
2023-03-02 15:17:48 +0530

    delted io_user_directory

--- SCRIPTS/COMMON/io_user_directory.py
+++
@@ -1,6 +0,0 @@
-# This is the local user directory, should be ignored while pushing to git repo
-
-input_common_dir = 'F:\\qa_automation\\PythonWorkingScripts_InputData\\'
-output_common_dir = 'F:\\qa_automation\\PythonWorkingScripts_Output\\'
-chrome_driver_dir = 'F:\\qa_automation\\'
-new = ""
