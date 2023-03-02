43da7665d2fe4503d537b25cafbf582d2ec28759
muthumurugan <muthumurugan.ramalingam@hirepro.in>
2023-03-02 15:01:14 +0530

    git ignore chaanges

--- SCRIPTS/COMMON/io_user_directory.py
+++ SCRIPTS/COMMON/io_user_directory.py
@@ -3,3 +3,4 @@
 input_common_dir = 'F:\\qa_automation\\PythonWorkingScripts_InputData\\'
 output_common_dir = 'F:\\qa_automation\\PythonWorkingScripts_Output\\'
 chrome_driver_dir = 'F:\\qa_automation\\'
+new = ""
