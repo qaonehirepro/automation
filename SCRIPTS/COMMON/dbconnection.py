e84386e76fac77f8c9828e0efaa604849fc62394
muthumurugan <muthumurugan.ramalingam@hirepro.in>
2023-03-21 12:14:57 +0530

    Removed unwanted lines from db connection script

--- SCRIPTS/COMMON/dbconnection.py
+++ SCRIPTS/COMMON/dbconnection.py
@@ -8,12 +8,5 @@ def ams_db_connection():
                                    database=amsin_master.get('database'),
                                    user=amsin_master.get('user'),
                                    password=amsin_master.get('password'))
-    # cursor = conn.cursor()
-    # query = 'select first_name, middle_name,last_name, candidate_name,email1,email2,mobile1,' \
-    #         'phone_office,pan_card,passport,aadhaar_no,usn,phone_residence from candidates where id=1324791;'
-    # cursor.execute(query)
-    # data = cursor.fetchone()
-    # print(data)
-    #conn.close()
     return conn
 
